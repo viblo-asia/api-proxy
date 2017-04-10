f67c19aff9452e25eb288754665e35660411a4c6
Roman Kinyakin <1@grep.su>
2017-04-10 15:21:35 +0700

    Fixes

--- nodejs/session.js
+++ nodejs/session.js
@@ -3,14 +3,22 @@ import redisConnection from 'connect-redis'
 
 const RedisStore = redisConnection(session)
 
-export default session({
-    secret: process.env.APP_KEY,
+const store = new RedisStore({
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || '6379',
+    pass: process.env.REDIS_PASSWORD || null,
+})
+
+const cookie = {
+    maxAge: 86400000,
+}
+
+let SessionMiddleware = session({
+    secret: process.env.NODE_KEY,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 86400000 },
-    store: new RedisStore({
-        host: process.env.REDIS_HOST || '127.0.0.1',
-        port: process.env.REDIS_PORT || '6379',
-        pass: process.env.REDIS_PASSWORD || null,
-    }),
+    cookie,
+    store,
 })
+
+export default SessionMiddleware

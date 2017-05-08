138a91e97f88fb9181822b2ecbdd4ca422ef8692
Roman Kinyakin <1@grep.su>
2017-05-08 15:23:33 +0700

    Update sessions configuration

---
+++ backpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+    serverSrcPath: path.join(__dirname, 'nodejs'),
+
+    webpack: (config, options, webpack) => {
+        config.entry.main = './server.js'
+        return config
+    },
+}
--- nodejs/proxy.js
+++ nodejs/proxy.js
@@ -1,11 +1,12 @@
 const api = require('./config').api
+const proxy = require('http-proxy-middleware')
 
-module.exports = require('http-proxy-middleware')('/api', {
+module.exports = proxy('/api', {
     target: api.url,
     pathRewrite: {
         '^/api': ''
     },
-    onProxyReq: (proxyReq, req, res) => {
+    onProxyReq (proxyReq, req, res) {
         let auth = (req.session && req.session.token)
             ? `${req.session.token.token_type} ${req.session.token.access_token}`
             : ''
